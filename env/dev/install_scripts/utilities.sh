564b50c826c9165dcf268dd47b2d4e4a847e7c7f
Jafar Sadik <dzafar.sadik@gmail.com>
2017-08-20 20:39:23 +0100

    Configuring Vagrant: changed domain to 'weemaps.com', fixed typo

--- env/dev/install_scripts/utilities.sh
+++ env/dev/install_scripts/utilities.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# dos2unit - converts files with DOC/MAC/WIN line breaks to Unix line breaks
+# dos2unix - converts files with DOC/MAC/WIN line breaks to Unix line breaks
 sudo apt-get install --yes --force-yes dos2unix
 
 # httpie - http client, a better alternative to curl or wget
