1f512ace1552e1f0a708178aebccfdc7da7dc2f0
Jafar Sadik <dzafar.sadik@gmail.com>
2017-09-04 13:21:38 +0100

    Initial map page with mock data

--- src/main/java/com/github/weewar/mapviewer/controllers/MapPageController.java
+++ src/main/java/com/github/weewar/mapviewer/controllers/MapPageController.java
@@ -1,19 +1,27 @@
 package com.github.weewar.mapviewer.controllers;
 
+import com.github.weewar.mapviewer.views.MapPageView;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MapPageController {
     @GetMapping({"/map/{map_id}", "/map/{map_id}/{map_name}"})
-    public String mapPage(@PathVariable("map_id") Integer mapId,
-                          @PathVariable(value = "map_name", required = false) String mapName,
-                          Model model) {
-        model.addAttribute("id", mapId);
-        model.addAttribute("name", mapName);
-        return "map-page";
+    public ModelAndView mapPage(@PathVariable("map_id") Integer mapId,
+                                @PathVariable(value = "map_name", required = false) String urlEncodedMapName) {
+        MapPageView mapPageView = new MapPageView();
+        mapPageView.mapId = mapId;
+        mapPageView.mapName = "Three ways";
+        mapPageView.revision = 1;
+        mapPageView.creator = "bert";
+        mapPageView.players = 2;
+        mapPageView.startCredits = 100;
+        mapPageView.income = 200;
+        mapPageView.width = 25;
+        mapPageView.height = 20;
+        return new ModelAndView("map-page", "map", mapPageView);
     }
 
     @GetMapping("/map")
---
+++ src/main/java/com/github/weewar/mapviewer/views/MapPageView.java
@@ -0,0 +1,13 @@
+package com.github.weewar.mapviewer.views;
+
+public class MapPageView {
+    public long mapId;
+    public String mapName;
+    public int revision;
+    public String creator;
+    public int players;
+    public int startCredits;
+    public int income;
+    public int width;
+    public int height;
+}
