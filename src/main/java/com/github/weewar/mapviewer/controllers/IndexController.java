f796c3a726735d386417f6342df52bd65fa83711
Jafar Sadik <dzafar.sadik@gmail.com>
2017-09-04 11:28:16 +0100

    Modified map page controller. Using a path variable instead of parameter. Accepting optional map name.

--- src/main/java/com/github/weewar/mapviewer/controllers/IndexController.java
+++ src/main/java/com/github/weewar/mapviewer/controllers/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String index() {
+    public String gotoSearchPage() {
         return "redirect:/search";
     }
 }
--- src/main/java/com/github/weewar/mapviewer/controllers/MapPageController.java
+++ src/main/java/com/github/weewar/mapviewer/controllers/MapPageController.java
@@ -3,13 +3,21 @@ package com.github.weewar.mapviewer.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MapPageController {
-    @GetMapping("/maps")
-    public String mapPage(@RequestParam(value = "id") String mapId, Model model) {
+    @GetMapping({"/map/{map_id}", "/map/{map_id}/{map_name}"})
+    public String mapPage(@PathVariable("map_id") Integer mapId,
+                          @PathVariable(value = "map_name", required = false) String mapName,
+                          Model model) {
         model.addAttribute("id", mapId);
+        model.addAttribute("name", mapName);
         return "map-page";
+    }
+
+    @GetMapping("/map")
+    public String gotoSearchPage() {
+        return "redirect:/search";
     }
 }
