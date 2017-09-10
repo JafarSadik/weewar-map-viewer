a45c03a1eaf276bef277fc5d0abdd8bf985647b2
Jafar Sadik <dzafar.sadik@gmail.com>
2017-09-10 10:54:21 +0100

    Display all ~12,000 maps on search page.

--- src/main/java/com/github/weewar/mapviewer/dao/WeewarMapDAO.java
+++ src/main/java/com/github/weewar/mapviewer/dao/WeewarMapDAO.java
@@ -3,10 +3,13 @@ package com.github.weewar.mapviewer.dao;
 import com.github.weewar.mapviewer.model.MapHeader;
 import com.github.weewar.mapviewer.model.WeewarMap;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WeewarMapDAO {
-    Optional<WeewarMap> getMapById(int mapId);
+    Optional<WeewarMap> findMapById(int mapId);
 
-    Optional<MapHeader> getMapHeaderById(int mapId);
+    Optional<MapHeader> findMapHeaderById(int mapId);
+
+    List<MapHeader> findMapHeaders(MapSearchCriteria searchCriteria);
 }
