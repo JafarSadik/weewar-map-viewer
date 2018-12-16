03dddf6d6ad502d920556aeccd008e015454250e
Jafar Sadik <dzafar.sadik@gmail.com>
2018-12-16 23:11:11 +0000

    Loading maps after spring context initialization in order to reduce application startup time and cope with Heroku slow startup errors.

--- src/main/kotlin/com/github/weewar/mapviewer/startup/PopulateInMemoryDAO.kt
+++ src/main/kotlin/com/github/weewar/mapviewer/startup/PopulateInMemoryDAO.kt
@@ -2,15 +2,16 @@ package com.github.weewar.mapviewer.startup
 
 import com.github.weewar.mapviewer.dao.memory.InMemoryWeewarMapDAO
 import org.slf4j.LoggerFactory.getLogger
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
-class PopulateInMemoryDAO {
+class PopulateInMemoryDAO(val inMemoryDao: InMemoryWeewarMapDAO) {
     private val logger = getLogger(PopulateInMemoryDAO::class.java)
 
-    @Autowired(required = false)
-    fun onStartup(inMemoryDao: InMemoryWeewarMapDAO) {
+    @EventListener(ApplicationReadyEvent::class)
+    fun onStartup() {
         logger.info("Populating in-memory repository")
         inMemoryDao.populate()
     }
