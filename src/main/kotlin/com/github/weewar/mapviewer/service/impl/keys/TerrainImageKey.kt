1042b93502a32cdbadb2265c137faf2282535be9
dsadik <dzafar.sadik@gmail.com>
2023-08-07 20:43:55 +0200

    Fixed some SonarLint issues

--- src/main/kotlin/com/github/weewar/mapviewer/service/impl/keys/TerrainImageKey.kt
+++ src/main/kotlin/com/github/weewar/mapviewer/service/impl/keys/TerrainImageKey.kt
@@ -1,6 +1,8 @@
 package com.github.weewar.mapviewer.service.impl.keys
 
-import com.github.weewar.mapviewer.model.enums.*
+import com.github.weewar.mapviewer.model.enums.Direction
+import com.github.weewar.mapviewer.model.enums.Owner
+import com.github.weewar.mapviewer.model.enums.TerrainType
 
 data class TerrainImageKey(
         val terrainType: TerrainType,
